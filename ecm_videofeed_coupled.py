eb2e03558198af6ed6729519c39ad1f651b5fd6d
AlexandreBanks6 <65726682+AlexandreBanks6@users.noreply.github.com>
2025-02-14 12:00:20 +0000

    Added Files from PC2

--- ecm_videofeed_coupled.py
+++ ecm_videofeed_coupled.py
@@ -12,7 +12,7 @@ Steps to Setup ROS for Cameras:
     1.2 $ssh pi@192.168.0.12
     1.3 $sudo ifconfig eth0 192.168.1.12 netmask 255.255.255.0 up
 
-    Don't close terminal!
+    Don't close terminal yet (not until step 8 below)!
 
 2. Connect white ethernet to wired ethernet
     - Under 'PCI Ethernet Connected' or 'wired ethernet':
@@ -35,11 +35,11 @@ Steps to Setup ROS for Cameras:
  7.4 roslaunch raspicam_node camerav2_1280x960.launch
     - Starts left external camera video feed with ros topic:
     'raspicam_node_left/image/compressed'
-
-8. ssh pi@192.168.1.12 => Enters right pi
- 8.2 export ROS_IP=192.168.1.12
- 8.3 export ROS_MASTER_URI=http://192.168.1.13:11311
- 8.4 roslaunch raspicam_node camerav2_1280x960.launch
+8. Close terminal window from step 1 where we did $ssh pi@192.168.0.12
+9. ssh pi@192.168.1.12 => Enters right pi
+ 9.2 export ROS_IP=192.168.1.12
+ 9.3 export ROS_MASTER_URI=http://192.168.1.13:11311
+ 9.4 roslaunch raspicam_node camerav2_1280x960.launch
     - Starts right external camera video feed with ros topic:
     'raspicam_node_right/image/compressed'
 
@@ -53,6 +53,7 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 import Tkinter as tk
+import PickUpCamera_Calibration
 
 #ROS Topics
 RightECM_Topic='ubc_dVRK_ECM/right/decklink/camera/image_raw/compressed'
@@ -70,7 +71,7 @@ ECM_HEIGHT=int(round(SIZE_REDUCTION_PERCENTAGE_ECM*986))
 VIEW_WINDOW_WIDTH=1024
 VIEW_WINDOW_HEIGHT=768
 
-EXTERNAL_SEPARATION=10 #Pixel offset from center line for left (neg offset) and right (pos offset)
+EXTERNAL_SEPARATION=50 #Pixel offset from center line for left (neg offset) and right (pos offset)
 
 ZEROS_ARRAY=np.zeros((EXTERNAL_HEIGHT,EXTERNAL_WIDTH,3))
 MAX_ARRAY=255*np.ones((EXTERNAL_HEIGHT,EXTERNAL_WIDTH,3))
@@ -93,6 +94,8 @@ class ECMVideoStreamer:
         rospy.Subscriber(name=RightExternal_Topic,data_class=CompressedImage,callback=self.rightExternalCallback,queue_size=1,buff_size=2**20)
         rospy.Subscriber(name=LeftExternal_Topic,data_class=CompressedImage,callback=self.leftExternalCallback,queue_size=1,buff_size=2**20)
 
+        ###################Variables and Booleans Init
+
         #Frames Init
         self.external_frame_left = None
         self.external_frame_right = None 
@@ -101,9 +104,18 @@ class ECMVideoStreamer:
 
 
         #Variabels to change with GUI
-        self.sharpness_kernel_value=7
-        self.sharpness_sigma_value=1
+        self.sharpness_kernel_value=15
+        self.sharpness_sigma_value=2
         self.external_separation_val=EXTERNAL_SEPARATION
+
+        #toggles external boolean
+        self.is_external_on=True
+
+        #Object to undistort frames
+        self.undistortion_obj=PickUpCamera_Calibration.PickUpUndistortion()
+        #Gets the camera params
+        self.is_undistort_available=self.undistortion_obj.getCameraParams()
+        self.is_undistort_on=True   #Toggles undistort on
 
         #Create slider window
         #self.dummy_img=np.zeros((200,600,3),np.uint8)
@@ -118,7 +130,7 @@ class ECMVideoStreamer:
 
         #Kernel Value for Sharpening
         self.kernel_label=tk.Label(self.gui_window,text='Sharpness Kernel: ').grid(row=0,column=0,sticky="nsew")
-        self.slider_kernel=tk.Scale(self.gui_window,from_=0,to=21,orient="horizontal")
+        self.slider_kernel=tk.Scale(self.gui_window,from_=0,to=25,orient="horizontal")
         self.slider_kernel.set(self.sharpness_kernel_value)
         self.slider_kernel.bind("<ButtonRelease-1>",self.kernelTrackbarCallback)
         self.slider_kernel.grid(row=0,column=1,sticky="nsew")
@@ -132,10 +144,19 @@ class ECMVideoStreamer:
 
         #Kernel Value for Sharpening
         self.separation_label=tk.Label(self.gui_window,text='External Cam. Separation: ').grid(row=2,column=0,sticky="nsew")
-        self.slider_separation=tk.Scale(self.gui_window,from_=-30,to=30,orient="horizontal")
+        self.slider_separation=tk.Scale(self.gui_window,from_=-100,to=100,orient="horizontal")
+
         self.slider_separation.set(self.external_separation_val)
         self.slider_separation.bind("<ButtonRelease-1>",self.separationTrackbarCallback)
         self.slider_separation.grid(row=2,column=1,sticky="nsew")
+
+        #Button to turn on/off undistortion (default is on)
+        self.toggle_undistortion_button=tk.Button(self.gui_window,text="Toggle Undistortion (default on)",command=self.toggleUndistortion)
+        self.toggle_undistortion_button.grid(row=3,column=0,sticky='nsew')
+
+        #Button to turn on/off external camera view
+        self.toggle_external_button=tk.Button(self.gui_window,text="Toggle External Camera",command=self.toggleExternalCallback)
+        self.toggle_external_button.grid(row=4,column=0,sticky='nsew')
 
 
         #main loop
@@ -152,6 +173,13 @@ class ECMVideoStreamer:
 
     def separationTrackbarCallback(self,event):
         self.external_separation_val=self.slider_separation.get()
+
+    def toggleExternalCallback(self):
+        self.is_external_on=not self.is_external_on
+
+    def toggleUndistortion(self):
+        self.is_undistort_on=not self.is_undistort_on
+        print("Undistortion On: "+str(self.is_undistort_on))
 
 
     def showFramesCallback(self):
@@ -188,11 +216,18 @@ class ECMVideoStreamer:
     def superimposeView(self,ecm_frame,external_frame,left_right):
         #Input: ecm frame (ecm_frame); external camera frame: external_frame
         #Output: ecm frame with external camera frame on bottom right below ecm (superimposed_frame)
-        if external_frame is not None:
+        if external_frame is not None and self.is_external_on:
             ecm_frame_new=cv2.resize(ecm_frame,(ECM_WIDTH,ECM_HEIGHT),interpolation=cv2.INTER_LINEAR)
             external_frame_new=cv2.resize(external_frame,(EXTERNAL_WIDTH,EXTERNAL_HEIGHT),interpolation=cv2.INTER_LINEAR)
+            # h,w=external_frame_new.shape[:2]
+            # print("h: "+str(h)+" w: "+str(w))
+            if self.is_undistort_available and self.is_undistort_on:               
+                external_frame_new=self.undistortion_obj.undistortPickupCameraFrames(external_frame_new,left_right)
+                # h,w=external_frame_new.shape[:2]
+                # print("h: "+str(h)+" w: "+str(w))
+                #external_frame_new=cv2.resize(external_frame_new,(EXTERNAL_WIDTH,EXTERNAL_HEIGHT),interpolation=cv2.INTER_LINEAR) #This line slows down the code a lot
             external_frame_new=self.unsharp_mask(external_frame_new,kernel_size=(self.sharpness_kernel_value,self.sharpness_kernel_value),\
-                                                 sigma=self.sharpness_sigma_value,amount=3,threshold=0)
+                                                  sigma=self.sharpness_sigma_value,amount=3,threshold=0)
             
             superimposed_frame=np.zeros((VIEW_WINDOW_HEIGHT,VIEW_WINDOW_WIDTH,3),np.uint8)
             superimposed_frame[0:ECM_HEIGHT,int(round((VIEW_WINDOW_WIDTH-ECM_WIDTH)/2)):int(round((VIEW_WINDOW_WIDTH-ECM_WIDTH)/2))+ECM_WIDTH]=ecm_frame_new
